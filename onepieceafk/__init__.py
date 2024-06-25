7d9d3b1e8127612037fe1a27906cb1a27b381427
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-06-25 04:06:31 -0400

    Update __init__.py

--- onepieceafk/__init__.py
+++ onepieceafk/__init__.py
@@ -1,6 +1,4 @@
 from .onepieceafk import OnePieceAFK
 
-__red_end_user_data_statement__ = "This cog stores user data including adventure status, bounties, crews, abilities, inventory, and game progress. All data can be removed upon user request."
-
 async def setup(bot):
     await bot.add_cog(OnePieceAFK(bot))
