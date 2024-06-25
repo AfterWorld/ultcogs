c5a41e2589f6f08ad81f7b3f3d7341cce5b9bf3d
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-06-25 00:12:41 -0400

    Create __init__.py

---
+++ New folder (2)/__init__.py
@@ -0,0 +1,6 @@
+from .customafk import CustomAFK
+
+__red_end_user_data_statement__ = "This cog stores user data including adventure status, bounties, crews, abilities, inventory, and game progress. All data can be removed upon user request."
+
+async def setup(bot):
+    await bot.add_cog(CustomAFK(bot))
