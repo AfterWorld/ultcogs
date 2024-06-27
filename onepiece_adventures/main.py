ef4fa22835d9cdb4ebe4bc2f9d55222193af4d85
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-06-26 20:03:03 -0400

    Update main.py

--- onepiece_adventures/main.py
+++ onepiece_adventures/main.py
@@ -136,7 +136,7 @@ class OnePieceAdventures(commands.Cog):
         """Challenge another user to a Davy Back Fight."""
         if opponent == ctx.author:
             return await ctx.send("You can't challenge yourself to a Davy Back Fight!")
-        await self.davy_back_fight_system.start_davy_back_fight(ctx, opponent)
+        await self.davy_back_fight.start_davy_back_fight(ctx, opponent)
 
     @commands.command()
     async def eat_devil_fruit(self, ctx, fruit_name: str):
