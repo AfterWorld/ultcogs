c8ccfd49ede7fab7aaacb4d022491a40ddcf6e54
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-13 18:10:44 -0500

    Update __init__.py

--- deathmatch/__init__.py
+++ deathmatch/__init__.py
@@ -1,1 +1,9 @@
+from redbot.core.bot import Red
+from .deathmatch import Deathmatch
+
+
+async def setup(bot: Red):
+    """Load the Deathmatch cog."""
+    cog = Deathmatch(bot)
+    await bot.add_cog(cog)
 
