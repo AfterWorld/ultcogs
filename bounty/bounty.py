ba0f2563da3b3bf01f7f646d8064a7b6dfa4e580
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-16 20:25:56 -0500

    Update bounty.py

--- bounty/bounty.py
+++ bounty/bounty.py
@@ -116,10 +116,10 @@ class BountyCog(commands.Cog):
         except OSError:
             return "Failed to load font. Please ensure the font file exists and is accessible."
 
-        poster_image.paste(avatar_image, (65, 250))
+        poster_image.paste(avatar_image, (65, 255))
 
         draw.text((150, 800), username, font=font, fill="black")
-        draw.text((150, 1000), f"{bounty_amount:,}", font=font, fill="black")
+        draw.text((150, 600), f"{bounty_amount:,}", font=font, fill="black")
 
         output = io.BytesIO()
         poster_image.save(output, format="PNG")
