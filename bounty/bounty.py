f6da9eeb1e834d27382afac166160ed4842768e7
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-16 20:18:04 -0500

    Update bounty.py

--- bounty/bounty.py
+++ bounty/bounty.py
@@ -108,7 +108,7 @@ class BountyCog(commands.Cog):
                 poster_data = await response.read()
 
         poster_image = Image.open(io.BytesIO(poster_data))
-        avatar_image = Image.open(io.BytesIO(avatar_data)).resize((300, 700))
+        avatar_image = Image.open(io.BytesIO(avatar_data)).resize((700, 300))
 
         draw = ImageDraw.Draw(poster_image)
         try:
