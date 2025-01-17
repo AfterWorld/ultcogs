02d2f0baa38cb3aee077f2d35039750ccd41991a
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-16 23:47:42 -0500

    Delete rpg directory

--- rpg/__init__.py
+++
@@ -1,8 +0,0 @@
-from redbot.core.bot import Red
-from .onepiece import OnePieceRPG
-
-
-async def setup(bot: Red):
-    """Load the RPG cog."""
-    cog = OnePieceRPG(bot)
-    await bot.add_cog(cog)
