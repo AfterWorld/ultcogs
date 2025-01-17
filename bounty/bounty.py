caf7e1ef899e9c90839e8b2c5706660141e3361a
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-16 20:32:05 -0500

    Update bounty.py

--- bounty/bounty.py
+++ bounty/bounty.py
@@ -108,7 +108,7 @@ class BountyCog(commands.Cog):
                 poster_data = await response.read()
 
         poster_image = Image.open(io.BytesIO(poster_data))
-        avatar_image = Image.open(io.BytesIO(avatar_data)).resize((625, 440))
+        avatar_image = Image.open(io.BytesIO(avatar_data)).resize((625, 445))
 
         draw = ImageDraw.Draw(poster_image)
         try:
@@ -118,7 +118,7 @@ class BountyCog(commands.Cog):
 
         poster_image.paste(avatar_image, (65, 235))
 
-        draw.text((150, 800), username, font=font, fill="black")
+        draw.text((150, 700), username, font=font, fill="black")
         draw.text((150, 870), f"{bounty_amount:,}", font=font, fill="black")
 
         output = io.BytesIO()
