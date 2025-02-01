3cd175668bbaf607e073467d0d0b19f2e8be80ec
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-01-31 21:55:16 -0500

    Delete bounty directory

--- bounty/__init__.py
+++
@@ -1,9 +0,0 @@
-from redbot.core.bot import Red
-from .bounty import BountyCog
-
-
-async def setup(bot: Red):
-    """Load the Deathmatch cog."""
-    cog = BountyCog(bot)
-    await bot.add_cog(cog)
-
