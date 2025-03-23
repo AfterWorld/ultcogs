476d3a3ca3bcd55391996f917cfce7752986a8da
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-23 15:00:26 -0500

    Update __init__.py

--- crewbattles/__init__.py
+++ crewbattles/__init__.py
@@ -1,7 +1,6 @@
 from redbot.core.bot import Red
 from .crew import CrewManagement
 
-async def setup(bot: Red):
-    """Load the CrewSystem cog."""
-    cog = CrewSystem(bot)  # Instantiate the main cog
-    await bot.add_cog(cog)
+def setup(bot):
+    cog = CrewManagement(bot)  # Updated to use CrewManagement instead of CrewSystem
+    bot.add_cog(cog)
