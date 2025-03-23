76253983e16fc710bac0113e68ab36155c9925df
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-03-23 17:17:02 -0500

    Update __init__.py

--- crewbattles/__init__.py
+++ crewbattles/__init__.py
@@ -1,13 +1,5 @@
-from redbot.core.bot import Red
 from .crew import CrewManagement
-from .tournament import TournamentSystem
 
 async def setup(bot):
-    crew_cog = CrewManagement(bot)
-    bot.add_cog(crew_cog)
-    
-    tournament_cog = TournamentSystem(bot)
-    bot.add_cog(tournament_cog)
-    
-    # Connect the cogs
-    tournament_cog.set_crew_manager(crew_cog)
+    cog = CrewManagement(bot)
+    await bot.add_cog(cog)
