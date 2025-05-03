763b967d65200d6330e25b144f381c6eab7cbf94
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-03 17:38:28 -0500

    Delete bountybattle/__init__.py

--- bountybattle/__init__.py
+++
@@ -1,7 +0,0 @@
-from redbot.core.bot import Red
-from .bountybattle import BountyBattle  # Ensure this matches the class name in bountybattle.py
-
-async def setup(bot: Red):
-    """Load the OnePieceRPG cog."""
-    cog = BountyBattle(bot)  # Instantiate the correct class
-    await bot.add_cog(cog)
