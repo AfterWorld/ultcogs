df43a6cb52dab1541226580b05ef4e50552ad0c6
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-04 18:06:31 -0500

    Update __init__.py

--- pokemon/__init__.py
+++ pokemon/__init__.py
@@ -1,10 +1,8 @@
-from redbot.core.bot import Red
-
 from .pokemon import PokemonCog
 
-
-async def setup(bot: Red) -> None:
-    """Set up the Pokemon cog."""
+async def setup(bot):
     cog = PokemonCog(bot)
     await bot.add_cog(cog)
     
+    # Start background task to check temporary forms
+    bot.loop.create_task(cog.check_temporary_forms())
