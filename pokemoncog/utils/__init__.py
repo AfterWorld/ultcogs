c091dac7a36074ad2118d43ab4243d8297e59e26
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-04 19:27:29 -0500

    Update __init__.py

--- pokemoncog/utils/__init__.py
+++ pokemoncog/utils/__init__.py
@@ -4,10 +4,10 @@ This package contains various utility functions used by the Pokemon cog:
 
 - api.py: Functions for interacting with the PokeAPI
 - formatters.py: Functions for formatting Pokemon data for display
-- spawn.py: Functions for Pokemon spawning mechanics
+- formatting/spawn.py: Functions for Pokemon spawning mechanics
 """
 
 # Import functions directly rather than using relative imports
 from .api import fetch_pokemon, fetch_all_forms, get_random_pokemon_id
 from .formatters import format_pokemon_name, create_spawn_embed
-from .spawn import spawn_pokemon, expire_spawn, add_pokemon_to_user, is_correct_catch
+from .formatting.spawn import spawn_pokemon, expire_spawn, add_pokemon_to_user, is_correct_catch
