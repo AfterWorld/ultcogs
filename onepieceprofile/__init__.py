d3aa884e6a6f01cbdbc22acdd90f7e445ad610ff
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-05 10:56:38 -0500

    Update __init__.py

--- onepieceprofile/__init__.py
+++ onepieceprofile/__init__.py
@@ -3,4 +3,5 @@ from .onepieceprofile import OnePieceProfile
 __red_end_user_data_statement__ = "This cog does not persistently store data or metadata about users."
 
 async def setup(bot):
-    await bot.add_cog(OnePieceProfile(bot))
+    cog = OnePieceProfile(bot)
+    await bot.add_cog(cog)
