1580661b60d4157fde45e42b06c9cbec8f11e488
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-04 19:21:46 -0500

    Delete pokemon directory

--- pokemon/__init__.py
+++
@@ -1,8 +0,0 @@
-from .pokemon import PokemonCog
-
-async def setup(bot):
-    cog = PokemonCog(bot)
-    await bot.add_cog(cog)
-    
-    # Start background task to check temporary forms
-    bot.loop.create_task(cog.check_temporary_forms())
