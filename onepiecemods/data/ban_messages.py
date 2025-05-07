4011f52307b9fb9335af41ca29e41916c4ad134e
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-07 17:30:11 -0500

    Create ban_messages.py

---
+++ onepiecemods/data/ban_messages.py
@@ -0,0 +1,36 @@
+# List of ban messages that will be randomly selected
+BAN_MESSAGES = [
+    "{mod} used Conqueror's Haki to ban {user} from the server!",
+    "{mod} drew the Yonko Shanks' sword and banned {user}!",
+    "{user} has been sentenced to Impel Down by {mod}!",
+    "{mod} used Buster Call to permanently remove {user} from the server!",
+    "{user} has been banished to the depths of the Calm Belt by {mod}!",
+    "{mod} used Enma to cut down {user} and ban them from the server!",
+    "{user} has been caught by the Marines and banned by Fleet Admiral {mod}!",
+    "{mod} activated the Pluton to ban {user} from existence!",
+    "{user} has been sent to Davy Jones' Locker by Captain {mod}!",
+    "{mod} summoned the Sea Kings to devour {user} and ban them!"
+]
+
+# List of ban GIFs that will be randomly selected
+BAN_GIFS = [
+    "https://media.giphy.com/media/OtdBjmYnxCuME/giphy.gif",  # Shanks Haki
+    "https://media.giphy.com/media/xUOxfk3xAkGCYvhQAg/giphy.gif",  # One Piece battle
+    "https://media.giphy.com/media/j2Ni21qM4mADRUpVVD/giphy.gif",  # Kaido attack
+    "https://media.giphy.com/media/CchzkJJ6UrQmQ/giphy.gif",  # Whitebeard power
+    "https://media.giphy.com/media/QCJbmMtHZhIHUHoYT9/giphy.gif",  # Admiral Akainu
+    "https://media.giphy.com/media/pUzqJ899BA0O4/giphy.gif",  # Garp punch
+    "https://media.giphy.com/media/dxldWRmzQBRhJCbVgA/giphy.gif",  # Powerful attack
+    "https://media.giphy.com/media/QZVdubDMGtczLGIKKQ/giphy.gif",  # Blackbeard
+    "https://media.giphy.com/media/WVlRnpOm7WfWs5HaU4/giphy.gif",  # Big Mom
+    "https://media.giphy.com/media/YWb3YoY8TolYuMpRc8/giphy.gif"   # Kaido dragon
+]
+
+# Alternative command names for ban
+BAN_ALIASES = [
+    "garpfist",
+    "admiralban",
+    "yonkopunch",
+    "marineban",
+    "sendtoimbeldown"
+]
