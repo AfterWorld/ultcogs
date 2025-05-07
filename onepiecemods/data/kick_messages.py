ee5019e444d96a91365e9024e9bc0689ded7da7d
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-07 17:29:58 -0500

    Create kick_messages.py

---
+++ onepiecemods/data/kick_messages.py
@@ -0,0 +1,36 @@
+# List of kick messages that will be randomly selected
+KICK_MESSAGES = [
+    "{mod} used Gomu Gomu no Pistol to kick {user} out of the server!",
+    "{mod} kicked {user} with the power of the Straw Hat Pirates!",
+    "{mod} sent {user} flying with a powerful kick!",
+    "{user} has been kicked off the Going Merry by {mod}!",
+    "{mod} used Diable Jambe to kick {user} from the server!",
+    "{user} has been sent flying by {mod}'s powerful Haki!",
+    "{mod} used Gear Third to punch {user} out of the server!",
+    "{user} has been defeated and kicked by {mod}!",
+    "{mod} kicked {user} into the Calm Belt! See you later!",
+    "{user} got caught in {mod}'s Gomu Gomu no Storm and was kicked out!"
+]
+
+# List of kick GIFs that will be randomly selected
+KICK_GIFS = [
+    "https://media.giphy.com/media/arbHBoiUWUgmc/giphy.gif",  # Luffy punch
+    "https://media.giphy.com/media/GRM7Z2s6AougoR3rvv/giphy.gif",  # Luffy kick
+    "https://media.giphy.com/media/HFMPreKu5LfivtOmEI/giphy.gif",  # Sanji kick
+    "https://media.giphy.com/media/vBjLa5fFnIZXi/giphy.gif",  # Zoro attack
+    "https://media.giphy.com/media/M9R6dDjrWOeMw/giphy.gif",  # Luffy gear 2
+    "https://media.giphy.com/media/11p1o3yoAQ7Sne/giphy.gif",  # Luffy punch
+    "https://media.giphy.com/media/RkDO1jwSjDPztB7OPU/giphy.gif",  # Zoro attack
+    "https://media.giphy.com/media/1zl1pvAaNMYrm/giphy.gif",  # Luffy punch
+    "https://media.giphy.com/media/drxdCXiVlVT3i/giphy.gif",  # Sanji kick
+    "https://media.giphy.com/media/nSojVeCWoWJ8c/giphy.gif"   # Luffy gatling
+]
+
+# Alternative command names for kick
+KICK_ALIASES = [
+    "zoroshove",
+    "sanjikick",
+    "strawhatpunch",
+    "gumupunch",
+    "knockaway"
+]
