4dfd1bbb327645aea5d2d8fc723ff7a2bf1ebfb1
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-07 17:30:22 -0500

    Create mute_messages.py

---
+++ onepiecemods/data/mute_messages.py
@@ -0,0 +1,36 @@
+# List of mute messages that will be randomly selected
+MUTE_MESSAGES = [
+    "{mod} placed Sea Prism Stone handcuffs on {user} for {time}!",
+    "{user} has been silenced with Law's ROOM ability by {mod} for {time}!",
+    "{mod} used Calm-Calm Fruit powers to mute {user} for {time}!",
+    "{user} has been affected by Sugar's power and turned silent for {time}!",
+    "{mod} used Haki to suppress {user}'s voice for {time}!",
+    "{user} has been placed in Sea Prism Stone by {mod} for {time}!",
+    "{mod} used Doflamingo's strings to seal {user}'s mouth for {time}!",
+    "{user} was caught by {mod} and placed in a soundproof Birdcage for {time}!",
+    "{mod} used Gura Gura no Mi to shock {user} into silence for {time}!",
+    "{user} has been transported to the Silent Island by {mod} for {time}!"
+]
+
+# List of mute GIFs that will be randomly selected
+MUTE_GIFS = [
+    "https://media.giphy.com/media/VEhW57pLd9c4GZ7Qr0/giphy.gif",  # Law's room
+    "https://media.giphy.com/media/t4KFWvvCcxhba3Kliq/giphy.gif",  # Law ability
+    "https://media.giphy.com/media/L05HgB2h6qICCs5Cr5/giphy.gif",  # Sea prism stone
+    "https://media.giphy.com/media/eMuCLQC76g7LrqYTbC/giphy.gif",  # Doflamingo strings
+    "https://media.giphy.com/media/SqCiPxzZ4MXuGvpDoG/giphy.gif",  # Devil fruit power
+    "https://media.giphy.com/media/9JnRMIFMYAKpaHRXRF/giphy.gif",  # Sugar's power
+    "https://media.giphy.com/media/5R40Du7OkJl0nqcTve/giphy.gif",  # Silence
+    "https://media.giphy.com/media/NluZvDHrOm2B0M8uXu/giphy.gif",  # Mute effect
+    "https://media.giphy.com/media/1SvQfSNqX7EkM4m803/giphy.gif",  # Law shambles
+    "https://media.giphy.com/media/S24BP0vsDRQ3K/giphy.gif"   # Calm down ability
+]
+
+# Alternative command names for mute
+MUTE_ALIASES = [
+    "seaprism",
+    "roomcage",
+    "silencepirate",
+    "calmcalm",
+    "devilfruitmute"
+]
