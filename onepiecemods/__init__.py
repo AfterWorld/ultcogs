bc961d79d0181705ee29c9873d603dffaec6bda3
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-05-07 17:31:21 -0500

    Update __init__.py

--- onepiecemods/__init__.py
+++ onepiecemods/__init__.py
@@ -1,5 +1,10 @@
-# __init__.py
 from .onepiecemods import OnePieceMods
 
 async def setup(bot):
-    await bot.add_cog(OnePieceMods(bot))
+    """Load the OnePieceMods cog."""
+    cog = OnePieceMods(bot)
+    
+    # Start the background task when cog is loaded
+    bot.loop.create_task(cog.init_task())
+    
+    await bot.add_cog(cog)
