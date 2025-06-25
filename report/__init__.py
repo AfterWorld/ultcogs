27e384b7c93588fcb2c5fc532b75a04f46a28e85
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-06-24 20:04:46 -0500

    Update __init__.py

--- report/__init__.py
+++ report/__init__.py
@@ -1,6 +1,6 @@
-from .advancedreport import AdvancedReport
+# __init__.py
+from .report import Report
 
 async def setup(bot):
-    """Entry point for loading the cog"""
-    cog = AdvancedReport(bot)
-    await bot.add_cog(cog)
+    """Setup function called by Red when loading the cog"""
+    await bot.add_cog(Report(bot))
