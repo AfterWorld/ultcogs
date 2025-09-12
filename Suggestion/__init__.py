05cbbbaaeca30d06b1bde44ff8ddb38459f008c1
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2025-09-11 23:51:06 -0500

    Update __init__.py

--- Suggestion/__init__.py
+++ Suggestion/__init__.py
@@ -1,5 +1,4 @@
-# Suggestion/__init__.py
-from .suggestion import Suggestions
+from .suggestion import EnhancedSuggestions
 
 async def setup(bot):
-    await bot.add_cog(Suggestions(bot))
+    await bot.add_cog(EnhancedSuggestions(bot))
